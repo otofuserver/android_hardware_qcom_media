206fe993121047a304686697b337e8d3478273f0
otofuserver <otouhusan.live@gmail.com>
2025-08-20 23:56:44 +0900

    Revert "libstagefrighthw: Convert to blueprint"
    This reverts commit f6111f0dfcdd6f499deb4a81162befdf8823ec41.

--- Android.mk
+++ Android.mk
@@ -5,6 +5,7 @@ QCOM_MEDIA_ROOT := $(call my-dir)
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
 ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
 ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
